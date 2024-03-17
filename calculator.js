b93dfe638514ee39821420a492507e7e84451543
Leon Saracini <141871578+Donoti13@users.noreply.github.com>
2024-03-17 16:18:13 +0100

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,82 @@
+
+const oneBut = document.getElementById("one");
+oneBut.className = "one-but";
+
+
+const twoBut = document.getElementById("two");
+twoBut.className = "two-but";
+
+
+const threeBut = document.getElementById("three");
+threeBut.className = "three-but"; 
+
+const fourBut = document.getElementById("four");
+fourBut.className = "four-but"; 
+
+const fiveBut = document.getElementById("five");
+fiveBut.className = "five-but";
+
+const sixBut = document.getElementById("six");
+sixBut.className = "six-but";
+
+const sevenBut = document.getElementById("seven");
+sevenBut.className = "seven-but";
+
+const eightBut = document.getElementById("eight");
+eightBut.className = "eight-but";
+
+const nineBut = document.getElementById("nine");
+nineBut.className = "nine-but";
+
+const zeroBut = document.getElementById("zero");
+zeroBut.className = "zero-but";
+
+const plusBut = document.getElementById("plus");
+plusBut.className = "plus-but";
+
+const minusBut = document.getElementById("minus");
+minusBut.className = "minus-but";
+
+const multiplyBut = document.getElementById("multiply");
+multiplyBut.className = "multiply-but";
+
+const divideBut = document.getElementById("divide");
+divideBut.className = "divide-but";
+
+const equalBut = document.getElementById("equal");
+equalBut.className = "equal-but";
+
+
+
+
+function add(num1, num2) {
+
+    return operationResul = num1 + num2;
+
+};
+
+function substract(num1, num2) {
+
+    return operationResul = num1 - num2;
+};
+
+function multiply(num1, num2) {
+
+    return operationResul = num1 * num2;
+};
+
+function divide(num1, num2) {
+
+    return operationResul = num1 / num2;
+};
+
+
+var firstNumber = 0;
+var operator = "+" || "-" || "*" || "/";
+var secondNumber = 0;
+
+
+function operate() {
+
+
+}

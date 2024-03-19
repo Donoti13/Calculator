2e799bd2cb79f833411f60afd604d8df4c1d4f07
Leon Saracini <141871578+Donoti13@users.noreply.github.com>
2024-03-19 18:18:16 +0100

    calculator.css

--- calculator.js
+++ calculator.js
@@ -10,8 +10,14 @@ function appendToDisplay (input) {
 
 
 function calculate() {
-
+    try {
     display.value = eval(display.value);
+}
+    catch (error) {
+
+        display.value = "ERROR!"
+    }
+
 }
 
 
